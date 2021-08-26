76a681c17ebd235d8b7df7698ddd6269d2a1c996
vik182 <verodriguezrsoft@gmail.com>
2021-08-26 17:33:14 -0500

    Update sdk

--- MarioBross/desktop/src/com/brentaureli/mariobros/desktop/DesktopLauncher.java
+++ MarioBross/desktop/src/com/brentaureli/mariobros/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.brentaureli.mariobros.MarioBros;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.forceExit = false;
 		config.width = 1200;
 		config.height = 624;
 		new LwjglApplication(new MarioBros(), config);
